bc21dac1b4ccf97307db8c78c55f2cbf3c633744
Juan Nunez <51241293+nunju9a@users.noreply.github.com>
2019-07-28 09:42:06 -0400

    update app.js to require express/data.json and pug

--- app.js
+++ app.js
@@ -1,3 +1,13 @@
 //////////////
 App.js
 /////////////
+
+ 
+// ADDING VARIABLES TO REQUIRE EXPRESS AND DATA.JSON 
+const express = require("express");
+const app = express();
+const path = require("path");
+const data = require("./data.json");
+const projects = data.projects;
+
+app.set("view engine", "pug");                         // Setting view engine to pug
