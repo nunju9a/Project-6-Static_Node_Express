2d269bc706e8481b4a8e9994cc4ecaba378a61de
Juan Nunez <51241293+nunju9a@users.noreply.github.com>
2019-07-28 09:56:36 -0400

    set routes in app.js

--- app.js
+++ app.js
@@ -3,11 +3,35 @@ App.js
 /////////////
 
  
-// ADDING VARIABLES TO REQUIRE EXPRESS AND DATA.JSON 
+// ADDING VARIABLES TO REQUIRE EXPRESS AND DATA.JSON
 const express = require("express");
 const app = express();
 const path = require("path");
 const data = require("./data.json");
 const projects = data.projects;
+app.set("view engine", "pug");                                   // Setting view engine to pug
+app.set("views", path.join(__dirname, "views"));                // Setting views directory
 
-app.set("view engine", "pug");                         // Setting view engine to pug
+
+// SERVING THE STATIC FILES LOCATED IN THE PUBLIC FOLDER
+app.use("/static", express.static(path.join(__dirname, "public")));
+
+
+// SETTING 'INDEX' ROUTE TO RENDER 'HOME' PAGE
+app.get("/", (req, res) => {
+    res.render("index", { projects });
+});
+  
+
+// SETTING 'ABOUT' ROUTE TO RENDER 'ABOUT' PAGE
+  
+app.get("/about", (req, res) => {
+    res.render("about");
+});
+  
+
+// SETTING 'PROJECTS' ROUTE TO RENDER 'PROJECTS' PAGE
+app.get("/projects/:id", (req, res) => {
+    const { id } = req.params;
+    const project = projects[id];
+});
